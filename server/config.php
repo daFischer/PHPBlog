6e00dc2ca1f6364d175a5ae093859a1a5753af2f
daFischer <johannes.w.fischer@web.de>
2015-10-12 22:47:21 +0200

    Comments for a better understanding

--- server/config.php
+++ server/config.php
@@ -1,10 +1,19 @@
 <?php
 // Set a few configuration variables
-$directory = "/home/.../blog/"
+
+// Change this if you want to have the blog at a position
+// different from public_html/server/
 $folder = "server/";
+
+// Whether this setup is for testing new blog entries
 $testing = true;
+
+// This is where files outside of public_html should be.
+// Wouldn't want to publicly show passwords, right?
+$directory = ".../secret files/";
 
 // Comment these two lines out:
 echo "You need to configure your config.php first!";
 exit();
+
 ?>
